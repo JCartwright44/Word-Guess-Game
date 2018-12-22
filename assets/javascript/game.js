377157f37052bfa8d2c34470e2d8d1ca97ecbbd0
Jason Cartwright <jason.cartwright44@gmail.com>
2018-12-22 11:16:21 -0800

    adding names

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// Create arrays of each of the answers
+var bird = ['B', 'i', 'r', 'd']; 
+var johnson = ['J', 'o', 'h', 'n', 's', 'o', 'n'];
+var jordan = ['J', 'o', 'r', 'd', 'a', 'n'];
+var jabbar = ['J', 'a', 'b', 'b', 'a', 'r'];
