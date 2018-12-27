56c1a5dbd5cb22f175b2b9ef53cceb0ed7df4d59
Jason Cartwright <jason.cartwright44@gmail.com>
2018-12-26 22:36:26 -0800

    words added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,11 @@ var bird = ['B', 'i', 'r', 'd'];
 var johnson = ['J', 'o', 'h', 'n', 's', 'o', 'n'];
 var jordan = ['J', 'o', 'r', 'd', 'a', 'n'];
 var jabbar = ['J', 'a', 'b', 'b', 'a', 'r'];
+var barkley = ['B', 'a', 'r', 'k', 'l', 'e', 'y'];
+var baylor = ['B', 'a', 'y', 'l', 'o', 'r']; 
+var miller = ['M', 'i', 'l', 'l', 'e', 'r'];
+var garnett = ['G', 'a', 'r', 'n', 'e', 't', 't']; 
+var erving = ['E', 'r', 'v', 'i', 'n', 'g']; 
+var robinson = ['R', 'o', 'b', 'i', 'n', 's', 'o', 'n'];
+
+var currentWord
